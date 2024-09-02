b9eec95ba7a1fc053b7cd174ceb666ccdf840618
opilane <opilane@HP-CZC2349HT2>
2024-09-02 12:29:09 +0300

    Add project files.

---
+++ Menu.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace TaskMaster;
+
+internal class Menu : ContentPage
+{
+    List<ContentPage> pages = new List<ContentPage>() { new MainPage(), new Valgusfoor(0) };
+    List<string> txt = new List<string> { "Home", "Valgusfoor" };
+    public Menu() 
+    {
+        for (int i = 0; i < pages.Count; i++)
+        {
+            Button btn = new Button
+            {
+                Text = txt[i],
+                BackgroundColor = Color.FromRgb(20, 100, 200),
+                TextColor = Color.FromRgb(10, 20, 15),
+                FontFamily = "Hey Comic",
+                BorderWidth = 10,
+                ZIndex = i
+            };
+            btn.Clicked += Btn_Clicked;
+            Content = new StackLayout
+            {
+                Padding = 20,
+                Children = { btn }
+            };
+        }
+
+
+    }
+    private async void Btn_Clicked(object? sender, EventArgs e)
+    {
+        Button button = (Button)sender;
+        await Navigation.PushAsync(pages[button.ZIndex]);
+    }
+}
+
+
---
+++ Valgusfoor.xaml.cs
@@ -0,0 +1,56 @@
+﻿namespace TaskMaster;
+
+
+public partial class Valgusfoor : ContentPage 
+{
+    BoxView redLight, yellowLight, greenLight;
+    bool trafficLight = false;
+    List<BoxView> lights = new List<BoxView>() { redLight, yellowLight, greenLight };
+    public Valgusfoor(int k)
+    {
+        Title = "Valgusfoor";
+
+        for (int i = 0; i < 3; i++) {
+            redLight = new BoxView
+            {
+                Color = Colors.Red,
+                WidthRequest = 100,
+                HeightRequest = 100,
+                CornerRadius = 50,
+            };
+            yellowLight = new BoxView
+            {
+                Color = Colors.Yellow,
+                WidthRequest = 100,
+                HeightRequest = 100,
+                CornerRadius = 50,
+            };
+            greenLight = new BoxView
+            {
+                Color = Colors.Green,
+                WidthRequest = 100,
+                HeightRequest = 100,
+                CornerRadius = 50,
+            };
+        };
+        Content = new StackLayout
+        {
+            HorizontalOptions = LayoutOptions.Center,
+            VerticalOptions = LayoutOptions.Center,
+            Spacing = 10,
+            WidthRequest = 120,
+            HeightRequest = 320,
+            BackgroundColor = Colors.Black,
+            Children = { redLight, yellowLight, greenLight },
+        };
+
+        Button on_btn = new Button
+        {
+            Text = "ON",
+        };
+        Button off_btn = new Button
+        {
+            Text = "Off",
+        };
+    }
+}
