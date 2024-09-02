ad2dbd2e64ce5112b3de203f81d07d8e07df2bf5
darja <darja.portnova@gmail.com>
2024-09-02 13:14:30 +0300

    fixed for loop, added event for buttons

--- Valgusfoor.xaml.cs
+++ Valgusfoor.xaml.cs
@@ -1,39 +1,27 @@
 ﻿namespace TaskMaster;
-
-
 public partial class Valgusfoor : ContentPage 
 {
-    BoxView redLight, yellowLight, greenLight;
     bool trafficLight = false;
-    List<BoxView> lights = new List<BoxView>() { redLight, yellowLight, greenLight };
+    List<BoxView> lights;
     public Valgusfoor(int k)
     {
         Title = "Valgusfoor";
-
+        lights = new List<BoxView>(3);
         for (int i = 0; i < 3; i++) {
-            redLight = new BoxView
+            lights.Add(new BoxView
             {
-                Color = Colors.Red,
+                Color = Colors.Gray,
                 WidthRequest = 100,
                 HeightRequest = 100,
                 CornerRadius = 50,
-            };
-            yellowLight = new BoxView
-            {
-                Color = Colors.Yellow,
-                WidthRequest = 100,
-                HeightRequest = 100,
-                CornerRadius = 50,
-            };
-            greenLight = new BoxView
-            {
-                Color = Colors.Green,
-                WidthRequest = 100,
-                HeightRequest = 100,
-                CornerRadius = 50,
-            };
+            });
         };
-        Content = new StackLayout
+
+        BoxView redLight = lights[0];
+        BoxView yellowLight = lights[1];
+        BoxView greenLight = lights[2];
+
+        StackLayout lightStack = new StackLayout
         {
             HorizontalOptions = LayoutOptions.Center,
             VerticalOptions = LayoutOptions.Center,
@@ -44,13 +32,32 @@ public partial class Valgusfoor : ContentPage
             Children = { redLight, yellowLight, greenLight },
         };
 
+        if (trafficLight == true) 
+        { 
+
+        };
+
         Button on_btn = new Button
         {
             Text = "ON",
         };
+        on_btn.Clicked += on_btn_clicked;
+
         Button off_btn = new Button
         {
-            Text = "Off",
+            Text = "OFF",
         };
+
+        off_btn.Clicked += Off_btn_Clicked;
+    }
+
+    private void Off_btn_Clicked(object? sender, EventArgs e)
+    {
+        trafficLight = false;
+    }
+
+    private async void on_btn_clicked(object? sender, EventArgs e)
+    {
+        trafficLight = true;
     }
 }
